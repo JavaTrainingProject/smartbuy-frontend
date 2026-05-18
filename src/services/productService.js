5de7799fc871176c69dba6645b5e2f138a5490c2
SabavathAruna <gayathri.b@clientservertech.com>
2026-05-18 11:40:24 +0530

    JTP--83productpage

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,26 +1,85 @@
 import axiosInstance from "./axiosInstance";
 
-export const getAllProducts = (page = 0, size = 5) => {
-  return axiosInstance.get(`/products?page=${page}&size=${size}`);
+
+export const getAllProducts = (
+  page = 0,
+  size = 6
+) => {
+
+  return axiosInstance.get(
+    `/products?page=${page}&size=${size}`
+  );
 };
 
 
 export const getProductById = (id) => {
-  return axiosInstance.get(`/products/${id}`);
+
+  return axiosInstance.get(
+    `/products/${id}`
+  );
 };
 
 
-export const createProduct = (formData) => {
-  return axiosInstance.post(`/products/create`, formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
+export const createProduct = (
+  formData
+) => {
+
+  return axiosInstance.post(
+    `/products/create`,
+    formData,
+    {
+      headers: {
+        "Content-Type":
+          "multipart/form-data",
+      },
+    }
+  );
 };
 
 
-export const deleteProduct = (id) => {
-  return axiosInstance.delete(`/products/${id}`);
+export const updateProduct = (
+  id,
+  formData
+) => {
+
+  return axiosInstance.put(
+    `/products/${id}`,
+    formData,
+    {
+      headers: {
+        "Content-Type":
+          "multipart/form-data",
+      },
+    }
+  );
 };
 
-export const toggleProductStatus = (id, status) => {
-  return axiosInstance.patch(`/products/${id}/status?status=${status}`);
+
+export const deleteProduct = (
+  id
+) => {
+
+  return axiosInstance.delete(
+    `/products/${id}`
+  );
+};
+
+export const softDeleteProduct = (
+  id
+) => {
+
+  return axiosInstance.delete(
+    `/products/${id}`
+  );
+};
+
+
+export const toggleProductStatus = (
+  id,
+  status
+) => {
+
+  return axiosInstance.patch(
+    `/products/${id}/status?status=${status}`
+  );
 };
